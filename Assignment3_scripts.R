94d2c5627707db3bc36ea88173d60f08dc15b86e
YanW <Broke.w@gmail.com>
2015-11-01 14:15:06 -0500

    Inspect the relationship between heights and circ

--- Assignment3_scripts.R
+++ Assignment3_scripts.R
@@ -2,3 +2,5 @@
 heights <- c(27.75, 24.5, 25.5, 26, 25, 27.75, 26.5, 27, 26.75, 26.75, 27.5)
 circ <- c(17.5, 17.1, 17.1, 17.3, 16.9, 17.6, 17.3, 17.5, 17.3, 17.5, 17.5)
 data <- data.frame(heights, circ)
+#Inspect graphically the data
+plot(data$heights, data$circ)
