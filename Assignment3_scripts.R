4fd6fffb0062cd29abcb79afc21f9f5dcc19b359
YanW <Broke.w@gmail.com>
2015-11-01 14:38:17 -0500

    Model quantified

--- Assignment3_scripts.R
+++ Assignment3_scripts.R
@@ -4,3 +4,8 @@ circ <- c(17.5, 17.1, 17.1, 17.3, 16.9, 17.6, 17.3, 17.5, 17.3, 17.5, 17.5)
 data <- data.frame(heights, circ)
 #Inspect graphically the data
 plot(data$heights, data$circ)
+#Test the potential linear correlation between heights and circumferences
+cor.test(data$circ, data$heights)
+#Quantify the relationship
+m <- lm(data$circ ~ data$heights)
+abline(m)
